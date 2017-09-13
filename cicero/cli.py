9e594a6328bb24d87d66e573a7483a59a98047f7
Jyry Suvilehto <jyry.suvilehto@csc.fi>
2017-09-13 17:14:14 +0300

    default bind to localhost so OS X firewall doesn't hate cicero

--- cicero/cli.py
+++ cicero/cli.py
@@ -9,7 +9,7 @@ def parse_args():
     arg('--file', '-f', dest='filename', help='serve a local file')
     arg('--engine', dest='engine', help='rendering engine', default='remark-0.13.0')
     arg('--debug', dest='debug', action='store_true', default=False)
-    arg('--host', dest='host', default=os.environ.get('HOST', '0.0.0.0'))
+    arg('--host', dest='host', default=os.environ.get('HOST', '127.0.0.1'))
     arg('--port', dest='port', type=int, default=int(os.environ.get('PORT', 5000)))
 
     return parser.parse_args()
