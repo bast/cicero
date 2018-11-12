0367ee4ecdb389119909d415740d7619d7b1e592
Radovan Bast <bast@users.noreply.github.com>
2018-11-12 21:48:30 +0100

    list available engines

--- cicero/cli.py
+++ cicero/cli.py
@@ -1,13 +1,23 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import os
+import glob
+import ntpath
+
+
+def _get_list_of_engines():
+    engines = []
+    for path in glob.glob(os.path.join(os.path.dirname(__file__), 'static', 'engines', '*')):
+        engines.append(ntpath.basename(path))
+    return sorted(engines)
 
 
 def parse_args():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     arg = parser.add_argument
+    engines = ', '.join(_get_list_of_engines())
 
     arg('--file', '-f', dest='filename', help='serve a local file')
-    arg('--engine', dest='engine', help='rendering engine (for talks pre-v3 use remark-legacy)', default='remark-0.13.0')
+    arg('--engine', dest='engine', help='rendering engine (available: {0})'.format(engines), default='remark-0.14.0')
     arg('--debug', dest='debug', action='store_true', default=False)
     arg('--host', dest='host', default=os.environ.get('HOST', '127.0.0.1'))
     arg('--port', dest='port', type=int, default=int(os.environ.get('PORT', 5000)))
