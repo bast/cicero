31c10290a28741291cf56ca8778e5322adbf090c
Radovan Bast <bast@users.noreply.github.com>
2018-11-12 20:24:57 +0100

    version_info is unused

--- cicero/version.py
+++ cicero/version.py
@@ -1,10 +1,1 @@
-from packaging.version import parse
-from collections import namedtuple
-
-__version__ = '0.1.1b1'
-
-version_info = namedtuple('version_info', ['major', 'minor', 'micro'])
-
-_p = parse(__version__)
-
-version_info.major, version_info.minor, version_info.micro = _p.release
+__version__ = '1.0.0dev1'
